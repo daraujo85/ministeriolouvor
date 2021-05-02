e45e0e5bc4c0e6647956cffb8ed36c08fb7463df
Diego Araujo <jcresgate@hotmail.com>
2021-05-02 17:06:24 -0300

    Model video

--- Models/Video.cs
+++ Models/Video.cs
@@ -3,9 +3,8 @@
     public class Video
     {
         public string Titulo { get; set; }
-        public string Thumbnail { get; set; }
-        public string Url { get; set; }
         public string Descricao { get; internal set; }
+        public string Url { get; set; }
         public string LargeThumbnail { get; internal set; }
         public string SmallThumbnail { get; internal set; }
     }
